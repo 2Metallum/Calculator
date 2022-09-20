b249e2e228590f4ac9eab75fafe3b4cfbea09d8a
2Metallum <k.akshentsev04@mail.ru>
2022-09-20 12:34:47 +0500

    release v2

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,27 +10,35 @@ public class Calculator {
     }
     public static String calc(String input) throws Exception {
         String[] strings = input.split(" ");
-        if(arabicRomanCheck(strings)){
+        if(arabicRomanCheck(strings) && strings.length == 3  && rangeCheck(strings)){
             return mathOperations(strings);
         }else{
             throw new IllegalArgumentException();
         }
     }
-//        public static char findSign(String[] str){
-//        String s = str[1];
-//        if(s.equals("+")){
-//            return '+';
-//        }else if(s.equals("-")){
-//            return '-';
-//        }else if(s.equals("*")){
-//            return '*';
-//        }else if(s.equals("/")){
-//            return '/';
-//        }
-//    }
 
     public static boolean arabicRomanCheck(String[] numbers){
         return Converter.isRoman(numbers[0]) == Converter.isRoman(numbers[2]);
+    }
+
+    public static boolean rangeCheck(String[] numbers){
+            if(Converter.isRoman(numbers[0])){
+                int a = Converter.romanToInteger(numbers[0]);
+                int b = Converter.romanToInteger(numbers[2]);
+                if( a >= 1 && b >= 1 && a <= 10 && b <= 10){
+                    return true;
+                }else{
+                    throw new IllegalArgumentException();
+                }
+            }else{
+                int a = Integer.parseInt(numbers[0]);
+                int b = Integer.parseInt(numbers[2]);
+                if( a >= 1 && b >= 1 && a <= 10 && b <= 10){
+                    return true;
+                }else{
+                    throw new IllegalArgumentException();
+                }
+            }
     }
     public static String mathOperations(String[] numbers) throws Exception{
         if(!(Converter.isRoman(numbers[0]))){
--- src/Converter.java
+++ src/Converter.java
@@ -4,7 +4,9 @@ public enum Converter {
     V(5),
     IX(9),
     X(10),
+    XL(40),
     L(50),
+    XC(90),
     C(100);
 
     private int arabicNumber;
